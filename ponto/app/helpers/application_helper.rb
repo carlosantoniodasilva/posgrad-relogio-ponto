dbddd0d9cbe67abc5a37f2a955b6ece873e958dc
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2014-06-10 19:48:32 -0300

    Implementa layout do login e usuario padrão ao fazer setup do db

--- ponto/app/helpers/application_helper.rb
+++ ponto/app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
+  def app_title
+    'Udesc Ponto'
+  end
+
   def nav_item(text, url_options)
     css = 'active' if controller_name == url_options[:controller]
     content_tag :li, link_to(text, url_options), class: css
