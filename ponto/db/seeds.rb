e5806b68fd08f6f0c9cf57e4aa9aa2dd9e10b5f0
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2014-06-11 21:46:18 -0300

    Atualiza o seeds para criar um departamento/funcionario/usuario inicial

--- ponto/db/seeds.rb
+++ ponto/db/seeds.rb
@@ -1,1 +1,8 @@
-User.find_or_create_by(email: 'test@example.com') { |u| u.password = 'secret.123' }
+department = Department.find_or_create_by(name: 'Administrativo')
+employee = Employee.find_or_create_by(name: 'Administrador') { |e|
+  e.department = department
+}
+user = User.find_or_create_by(email: 'admin@example.com') { |u|
+  u.password = u.password_confirmation = 'secret.123'
+  u.employee = employee
+}
